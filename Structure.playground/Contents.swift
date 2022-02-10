669dbda8e665054e2151c4c7e7cee91f240f0914
Bala <balamurugan2999@gmail.com>
2022-02-10 22:12:48 +0530

    Swift Basics - Structure Init
    added Init method in Structure

--- Structure.playground/Contents.swift
+++ Structure.playground/Contents.swift
@@ -1,3 +1,23 @@
 import UIKit
 
-var greeting = "Hello, playground"
+struct Person {
+    
+    let name: String
+    let age: Int
+    let city: String
+    
+    init(name: String, age: Int, city: String?){
+        self.name = name
+        self.age = age
+        self.city = city ?? "chennai"
+    }
+}
+
+
+let people = Person(name: "bala", age: 23, city: nil)
+people.city
+people.name
+
+let maduraiPeople = Person(name: "ragul", age: 31, city: "")
+maduraiPeople.city
+maduraiPeople.name
