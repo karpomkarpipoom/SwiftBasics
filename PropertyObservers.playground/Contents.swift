e2185c0e6084bbbb728b93c51bfd658078adb2dd
Bala <33730090+karpomkarpipoom@users.noreply.github.com>
2022-12-13 20:05:45 +0530

    Property observer

---
+++ PropertyObservers.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class StepCounter {
+    var totalSteps: Int = 0 {
+        willSet(newTotalSteps) {
+            print("About to set totalSteps to \(newTotalSteps)")
+        }
+        didSet {
+            if totalSteps > oldValue  {
+                print("Added \(totalSteps - oldValue) steps")
+            }
+        }
+    }
+}
+
+
+let steps = StepCounter()
+
